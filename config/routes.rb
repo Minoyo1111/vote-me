b10f0f6d8ca264979835240857ff0694129e6453
Mino Chen <oil_chen@cycu.org.tw>
2020-11-14 10:46:16 +0800

    The remaining Bootstrap is useless

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
-  resources :candidates
+  resources :candidates do
+    member do
+      post :vote
+    end
+  end
+
+  root 'candidates#index'
+
 
   # get "/candidates", to "candidates#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
